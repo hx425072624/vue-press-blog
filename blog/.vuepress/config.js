9ac788a0656f7d9c2a9396bef8330517d2e57b25
huang xing <jindun@JinDundeiMac.local>
2019-11-21 15:00:25 +0800

    fix config base

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'VuePress Blog Example',
   description: 'This is a blog example built by VuePress',
-  base:'vue-press-blog',
+  base:'/vue-press-blog/',
   theme: '@vuepress/blog', // OR shortcut: @vuepress/blog
   themeConfig: {
     /**
